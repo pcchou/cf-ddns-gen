59f88c553c30b17f8c4ae1cee6508477d4bd5d86
Philip Chou <pcchou@pcchou.me>
2015-06-14 11:49:39 +0800

    use native modules, and fixed coding style

--- gen.py
+++ gen.py
@@ -5,23 +5,25 @@ By pcchou, Released under the MIT License. '''
 import json
 import os
 from time import time, sleep
-
-import requests
-from tabulate import tabulate
+from urllib import request, parse
 
 
-def subdomain_selector(a_records):
+def subdomain_selector(def_a_records):
+    """ A function for the users to select A record. """
     filtered_a_records = []
-    for (index, item) in enumerate(
-           [record["name"] for record in a_records]):
+    for (index, item) in enumerate([
+            record["name"] for record in def_a_records]):
         filtered_a_records.append([index, item])
     print("")
     print("Please select your desired A record:")
-    print(tabulate(filtered_a_records, headers=["#", "Name"]))
+    print("#   Name          \n"
+          "------------------")
+    for host in filtered_a_records:
+        print(str(host[0]) + " "*(3 - len(str(host[0]))) + " " + host[1])
     print("")
     answer = int(input("Number (#): "))
-    if answer in [filtered_a_records[number][0] for number,_ in enumerate(filtered_a_records)]:
-        return a_records[answer]["name"], a_records[answer]["rec_id"], a_records[answer]["ttl"]
+    if answer in [filtered_a_records[number][0] for number, _ in enumerate(filtered_a_records)]:
+        return A_RECORDS[answer]["name"], A_RECORDS[answer]["rec_id"], A_RECORDS[answer]["ttl"]
     else:
         raise ValueError("Not a vaild answer.")
 
@@ -31,77 +33,78 @@ CF_TOKEN = str(input("Please input your CloudFlare API Token. \n"
                      "https://www.cloudflare.com/a/account/my-account. \n"
                      "CloudFlare API Token: ").strip().lower())
 CF_EMAIL = str(input("Please input your CloudFlare account e-mail: ").strip().lower())
-CF_DOMAIN = str(input("Please input your CloudFlare target domain (not the subdomain): ").strip().lower())
+CF_DOMAIN = str(input(
+    "Please input your CloudFlare target domain (not the subdomain): ").strip().lower())
 print("\n"
       "Retrieving informations...")
 
 try:
-    parsed_list_req = json.loads(
-            requests.get("https://www.cloudflare.com/api_json.html", params={
-                    "a": "rec_load_all",
-                    "z": CF_DOMAIN,
-                    "tkn": CF_TOKEN,
-                    "email": CF_EMAIL}).text)
+    PARSED_LIST_REQ = json.loads(request.urlopen(
+        request.Request("https://www.cloudflare.com/api_json.html", parse.urlencode({
+            "a": "rec_load_all",
+            "z": CF_DOMAIN,
+            "tkn": CF_TOKEN,
+            "email": CF_EMAIL}).encode('utf-8'))).read().decode())
 except ConnectionError:
     raise ConnectionError("You need a internet connection.")
 
-if not parsed_list_req["result"] == "success":
+if not PARSED_LIST_REQ["result"] == "success":
     raise ValueError("You're credentials are incorrect.")
 
 
-a_records = []
-for record in parsed_list_req["response"]["recs"]["objs"]:
+A_RECORDS = []
+for record in PARSED_LIST_REQ["response"]["recs"]["objs"]:
     if record["type"] == "A":
-        a_records.append(record)
+        A_RECORDS.append(record)
 
-cf_id = ""
-while not cf_id:
+CF_ID = ""
+while not CF_ID:
     try:
-        cf_name, cf_id, cf_ttl = subdomain_selector(a_records)
+        CF_NAME, CF_ID, CF_TTL = subdomain_selector(A_RECORDS)
     except ValueError:
         print("Not a vaild answer.\n")
 
-script_path = "/tmp/cf-ddns-" + cf_name + "-" + str(int(time())) + ".sh"
-script = open(script_path, "w")
+SCRIPT_PATH = "/tmp/cf-ddns-" + CF_NAME + "-" + str(int(time())) + ".sh"
+SCRIPT = open(SCRIPT_PATH, "w")
 
-script_list = [
-        "#!/bin/bash",
-        "ip=`curl -s http://icanhazip.com`",
-        "if [ -f /tmp/.cf-oldip ]; then",
-        "    oldip=`cat /tmp/.cf-oldip`",
-        "else",
-        "    echo 'Old IP file not found, creating one.'",
-        "    oldip=''",
-        "fi",
-        "if [[ \"$ip\" == \"$oldip\" ]]; then",
-        "    echo 'IP unchanged, do nothing.'",
-        "else",
-        "    echo 'Updated IP to '\"$ip\"'.'",
-        "    echo $ip > /tmp/.cf-oldip",
-        ("    curl -s https://www.cloudflare.com/api_json.html"
-         " -d 'a=rec_edit' -d 'type=A'"
-         " -d \"content=$ip\""
-         " -d 'tkn=" + CF_TOKEN + "'"
-         " -d 'email=" + CF_EMAIL + "'"
-         " -d 'z=" + CF_DOMAIN + "'"
-         " -d 'name=" + cf_name + "'"
-         " -d 'id=" + cf_id + "'"
-         " -d 'ttl=" + cf_ttl + "'"
-         "|perl -pe 's/.*\"result\":\"(.*?)\".*/$1/'"),
-        "fi"]
+SCRIPT_LIST = [
+    "#!/bin/bash",
+    "ip=`curl -s http://icanhazip.com`",
+    "if [ -f /tmp/.cf-oldip ]; then",
+    "    oldip=`cat /tmp/.cf-oldip`",
+    "else",
+    "    echo 'Old IP file not found, creating one.'",
+    "    oldip =''",
+    "fi",
+    "if [[ \"$ip\" == \"$oldip\" ]]; then",
+    "    echo 'IP unchanged, do nothing.'",
+    "else",
+    "    echo 'Updated IP to '\"$ip\"'.'",
+    "    echo $ip > /tmp/.cf-oldip",
+    ("    curl -s https://www.cloudflare.com/api_json.html"
+     " -d 'a=rec_edit' -d 'type=A'"
+     " -d \"content=$ip\""
+     " -d 'tkn=" + CF_TOKEN + "'"
+     " -d 'email=" + CF_EMAIL + "'"
+     " -d 'z=" + CF_DOMAIN + "'"
+     " -d 'name=" + CF_NAME + "'"
+     " -d 'id=" + CF_ID + "'"
+     " -d 'ttl=" + CF_TTL + "'"
+     "|perl -pe 's/.*\"result\":\"(.*?)\".*/$1/'"),
+    "fi"]
 
-for line in script_list:
-    script.write("%s\n" % line)
-os.chmod(script_path, 0o0755)
+for line in SCRIPT_LIST:
+    SCRIPT.write("%s\n" % line)
+os.chmod(SCRIPT_PATH, 0o0755)
 sleep(2)
 
 print("\n")
 print("Done!\n"
-      "Script file written to \"" + script_path + "\".\n")
+      "Script file written to \"" + SCRIPT_PATH + "\".\n")
 
 print("You will need to move it to a persistent PATH directory (eg. /usr/local/bin/),\n"
       "Execute this command with the root user: \n"
-      "\"mv \"" + script_path + "\" /usr/local/bin/cf-ddns.sh\n\"")
+      "\"mv \"" + SCRIPT_PATH + "\" /usr/local/bin/cf-ddns.sh\n\"")
 
 print("You will also need to set it to be run automatically, by setting it in your crontab.\n"
       "Run \"crontab -e\" and add this line to the end:\n"
